fa87ae42a8299fecaf25ea623c59b6fe5520883d
Callum <callumk0104@gmail.com>
2022-03-25 19:19:11 +0000

    Created Garages DAO

---
+++ deploy/backend/src/daos/Garages.php
@@ -0,0 +1,99 @@
+<?php
+namespace kv6002\daos;
+
+use database\Field;
+
+use kv6002\domain;
+
+/**
+ * Allows retrieving garages
+ * 
+ * @author Callum
+ */
+class Garages{
+    private $db;
+
+    public funtion __construct($db){
+        $this->db = $db;
+    }
+
+    /**
+     * Return a Garage object for the Garage within the database 
+     * with a given ID 
+     * 
+     * @param int $id The ID of the Garage to fetch
+     * @return Garage A Garage object for that garage
+     */
+    public function getGarage($id){
+        return $this->db->fetch(
+            "SELECT Garage.id as id,"
+            ."   vts,"
+            ."   name,"
+            ."   ownerName,"
+            ."   emailAddress,"
+            ."   telephoneNumber,"
+            ."   paidUntil,"
+            ."   password,"
+            ."   passwordResetRequired"
+            ." FROM Garage"
+            ." JOIN User ON Garage.id = User.id"
+            ." WHERE id = :id",
+            ["id" => $id],
+            domain\Garage::class,
+            null,
+            [
+                new Field("vts"),
+                new Field("name"),
+                new Field("ownerName"),
+                new Field("emailAddress"),
+                new Field("telephoneNum"),
+                new Field("paidUntil"),
+                new Field("password"),
+                new Field("passwordResetRequired")
+
+            ]
+
+        );
+    } 
+
+    /**
+     * 
+     * Return all Garages in the database.
+     * 
+     * @return Garage A Garage object for that Garage
+     * 
+     */
+    public function getGarages(){
+        return $this->db->fetchAll(
+            "SELECT Garage.id as id,"
+            ."   vts,"
+            ."   name,"
+            ."   ownerName,"
+            ."   emailAddress,"
+            ."   telephoneNumber,"
+            ."   paidUntil,"
+            ."   password,"
+            ."   passwordResetRequired"
+            ." FROM Garage"
+            ." JOIN User ON Garage.id = User.id",
+            ["id" => $id],
+            domain\Garage::class,
+            null,
+            [
+                new Field("vts"),
+                new Field("name"),
+                new Field("ownerName"),
+                new Field("emailAddress"),
+                new Field("telephoneNum"),
+                new Field("paidUntil"),
+                new Field("password"),
+                new Field("passwordResetRequired")
+
+            ]
+
+        );
+    }
+
+    //TODO : Garage Creation
+
+}
