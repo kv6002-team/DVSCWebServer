8c3fd91bb9f56d55443dc79c82cba5b5bef0bf89
William Taylor <williamtaylordev@gmail.com>
2022-04-22 12:56:42 +0100

    Adjust footer

--- website/src/standard/Footer.jsx
+++ website/src/standard/Footer.jsx
@@ -15,9 +15,9 @@ export default class Footer extends react.Component {
   render() {
     return [
       <div className="footer-spacing"></div>,
-      <footer>
+      <footer className="bg-dark text-light">
         <Container>
-          <Row className="p-2">
+          <Row className="pb-3">
             <Col sm={1}></Col>
             <Col sm={3}>
               <p><strong>Email</strong>: contact@dvsc.services</p>
@@ -27,7 +27,7 @@ export default class Footer extends react.Component {
               <Link to="/legal/privacy-policy">Privacy Policy</Link>
             </Col>
           </Row>
-          <Row className="p-2 text-center darken">
+          <Row className="p-2 text-center block-emphasis">
             <p>Copyright &copy; Donaldsons' Vehicle Specialist Consultancy.</p>
           </Row>
         </Container>
