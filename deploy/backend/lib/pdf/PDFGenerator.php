4c48addfc50fbc034b03c995a46f797d2e09c2ff
Scrub <scott-donaldson@outlook.com>
2022-04-26 22:02:51 +0100

    Added dynamic report generation with FPDF

---
+++ deploy/backend/lib/pdf/PDFGenerator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace pdf;
+require('fpdf.php');
+
+class PDFGenerator extends \FPDF {
+  public function generate_header($title){
+    $this->Cell(30,10,$title,1,0,'C');
+  }
+  public function generate_table($headers, $array){
+    foreach($headers as $col){
+      $this->Cell(40, 6, $col, 1);
+      $this->Ln(10);
+    }
+    $this->Ln();
+    foreach($array as $data){
+      $this->Cell(40, 6, $data, 1);
+    }
+    $this->Ln();
+  }
+}
+?>
