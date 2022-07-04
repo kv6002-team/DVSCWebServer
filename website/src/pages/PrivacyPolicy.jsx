7fad7cb717a6207c083695b0f5c4f2d353e11cb4
William Taylor <williamtaylordev@gmail.com>
2022-07-04 16:29:14 +0100

    Fix main header and illegal HTML

--- website/src/pages/PrivacyPolicy.jsx
+++ website/src/pages/PrivacyPolicy.jsx
@@ -15,52 +15,52 @@ import './PrivacyPolicy.css';
 export default class PrivacyPolicy extends react.Component {
   render() {
     return (
-      <Main header="Donaldsons' Vehicle Specialist Consultancy Privacy Policy">
+      <Main>
         <Container>
-        <br/>
-        <h2>Contact Details</h2>
-        <p>
-          Name: Kevin Donaldson
-        </p>
-        <p>
-          Email: kevindonaldsondvsc@outlook.co.uk
-        </p>
-        <h2>Types of personal information we collect</h2>
-        <p>
-          We currently collect and process the following information:
+          <h1>Donaldsons' Vehicle Specialist Consultancy Privacy Policy</h1>
+
+          <h2>Contact Details</h2>
+          <p>Name: Kevin Donaldson</p>
+          <p>Email: kevindonaldsondvsc@outlook.co.uk</p>
+
+          <h2>Types of personal information we collect</h2>
+          <p>We currently collect and process the following information:</p>
           <ul>
             <li>Personal identifiers, contacts and characteristics (For example, name and contact details)</li>
           </ul>
-        </p>
-        <h2>How we collect personal information and why we have it</h2>
-        <p>
-          We do not collect information indirectly and only process information directly provided by you.
-          Most of the personal infomation we process is provided to us directly by you for one of the following reasons:
+
+          <h2>How we collect personal information and why we have it</h2>
+          <p>
+            We do not collect information indirectly and only process information directly provided by you.
+            Most of the personal infomation we process is provided to us directly by you for one of the following reasons:
+          </p>
           <ul>
             <li>Processing garage information to track instrument details</li>
             <li>Storing personal information required to contact you if required</li>
           </ul>
-          We use the personal infromation you have provided us to keep track of garage information, allowing us to perform our job
-          in ensuring that garage quality is sufficient according to a standards board.
+          <p>
+            We use the personal infromation you have provided us to keep track of garage information, allowing us to perform our job
+            in ensuring that garage quality is sufficient according to a standards board.
+          </p>
 
-          We will not share any of your information provided with any other 3rd-party organisation or individual.
+          <p>We will not share any of your information provided with any other 3rd-party organisation or individual.</p>
 
-          Under the UK General Data Protecection Regulation (UK GDPR), the lawfull bases we rely on for processing this information are:
+          <p>Under the UK General Data Protecection Regulation (UK GDPR), the lawfull bases we rely on for processing this information are:</p>
           <ul>
             <li>Your consent. You are able to remove your conset at any time. You can do this by contacting Kevin Donaldson at kevindonaldsondvsc@outlook.com.</li>
             <li>We have a contractual obligation.</li>
             <li>We have legitimate interest.</li>
           </ul>
-        </p>
-        <h2>How we store your information</h2>
-        <p>
-          Your information is stored securely on an offsite server located in the UK.
-          We keep your information for as long as you remain a customer.
-          Data is deleted by permenantly removing it from our service database immediately after you are no longer a customer.
-        </p>
-        <h2>Your data protection rights</h2>
-        <p>
-          Under data protection law, you have rights including:
+
+          <h2>How we store your information</h2>
+          <p>
+            Your information is stored securely on an offsite server located in the UK.
+            We keep your information for as long as you remain a customer.
+            Data is deleted by permenantly removing it from our service database immediately after you are no longer a customer.
+          </p>
+
+          <h2>Your data protection rights</h2>
+          <p>Under data protection law, you have rights including:</p>
           <ul>
             <li>
               <strong>Your right of access</strong>
@@ -90,13 +90,14 @@ export default class PrivacyPolicy extends react.Component {
               </p>
             </li>
           </ul>
-          Please contact us at: kevindonaldsondvsc@outlook.com or +44 07493904628
-        </p><br/>
-        <h2>How to complain</h2>
-        <p>
-          If you have any concerns about our use of your personal information, you can make a complaint to us at kevindonaldsondvsc@outlook.com or +44 07493904628.
-          You can also complain to the ICO if you are unhappy with how we have used your data.
-          The ICO's address:      
+          <p>Please contact us at: kevindonaldsondvsc@outlook.com or +44 07493904628</p>
+
+          <h2>How to complain</h2>
+          <p>
+            If you have any concerns about our use of your personal information, you can make a complaint to us at kevindonaldsondvsc@outlook.com or +44 07493904628.
+            You can also complain to the ICO if you are unhappy with how we have used your data.
+          </p>
+          <p>The ICO's address:</p>
           <ul id='ICO-Address'>
             <li>Information Commissioner's Office</li>
             <li>Wycliffe House</li>
@@ -108,7 +109,6 @@ export default class PrivacyPolicy extends react.Component {
             <li>Helpline number: 0303 123 1113</li>
             <li>ICO website: https://www.ico.org.uk</li>
           </ul>
-        </p>
         </Container>
       </Main>
     );
